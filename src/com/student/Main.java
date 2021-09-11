fc41ebe4138bbea2b39bb894ce7553e803b56448
LeeDonggyu <asasd456123@naver.com>
2021-09-11 19:11:37 +0900

    implement 'sort by name'

--- src/com/student/Main.java
+++ src/com/student/Main.java
@@ -1,10 +1,31 @@
 package com.student;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		List<Student> list = new ArrayList<Student>();
+		list.add(new Student(101, "Han", 23));
+		list.add(new Student(102, "Song", 16));
+		list.add(new Student(103, "Lee", 23));
+		list.add(new Student(104, "Kim", 27));
+		list.add(new Student(105, "Park", 19));
+		list.add(new Student(106, "Baek", 20));
+		list.add(new Student(107, "Oh", 21));
+		list.add(new Student(108, "Choi", 24));
+		list.add(new Student(109, "Jun", 28));
+		list.add(new Student(110, "Son", 24));
+		
+		Collections.sort(list);
+		
+		System.out.println("Student List (ordered by name)");
+		for(Student s : list) {
+			System.out.println(s.toString());
+		}
 	}
 
 }
--- src/com/student/Student.java
+++ src/com/student/Student.java
@@ -1,5 +1,27 @@
 package com.student;
 
-public class Student {
+public class Student implements Comparable<Student>{
+	int no;
+	String name;
+	int age;
+	
+	public Student(int no, String name, int age) {
+		this.no = no;
+		this.name = name;
+		this.age = age;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "Student [no=" + no + ", name=" + name + ", age=" + age + "]";
+	}
+
+
+	@Override
+	public int compareTo(Student o) {
+		// TODO Auto-generated method stub
+		return name.compareTo(o.name);
+	}
 
 }
